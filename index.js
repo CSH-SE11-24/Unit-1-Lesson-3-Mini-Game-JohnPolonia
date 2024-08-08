c50b585f9f007cfc344f219186c9403953ef2e51
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 10:25:48 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// Get started!
+let name = prompt("What's your name?")
+console.log("welcome To Aspores," + name)
+let name2 = name
+console.log( "Hello",  + name2,  "Welcome to my game this game you will be able to choose your own level and continue to game as a starter ")
+
+let number = prompt("Choose the level you would like to be")
+console.log("Thank you, Your level has now been set to", + number)
+constant 
